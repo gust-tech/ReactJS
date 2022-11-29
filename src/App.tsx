934b32a3bea5f24d4f70ccc63af3c9765c01a12d
gust-tech <gstvgeneration@gmail.com>
2022-11-29 08:32:43 -0300

    Envio

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Footer from './components/estaticos/footer/Footer';
+import Home from './components/estaticos/home/Home';
+import Navbar from './components/estaticos/navbar/Navbar';
+
+function App() {
+  return (
+    <>
+    <Navbar />
+    <Home />
+    <Footer />
+    </>
+  );
+}
+
+export default App;
